8834cd1ebbb2507e52e75ff754bd8f1ecd728523
Voitik <daniil.voytik@gmail.com>
2023-11-18 13:50:36 +0300

    Без файла ui, класс в коде

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 import random
 import sys
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Suprematism(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(300, 210, 181, 101))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 20))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Создать круг"))
+
+
+class Suprematism(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.coords = (100, 100)
         self.pushButton.clicked.connect(self.click)
 
     def draw(self, qp):
         r = random.randint(20, 100)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(*[random.randint(0, 255) for _ in range(3)]))
         qp.drawEllipse(int(self.coords[0] - r / 2), int(self.coords[1] - r / 2), r, r)
 
     def paintEvent(self, event):
